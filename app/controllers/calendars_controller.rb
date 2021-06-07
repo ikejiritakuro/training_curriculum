cf761934df0f9012f78d30296f40f768a8cc89fa
ikejiritakuro <talangchikao165@gmail.com>
2021-06-07 20:41:50 +0900

    Issue指摘修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -36,12 +36,12 @@ class CalendarsController < ApplicationController
       end
 
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[(@todays_date+x).wday]}
+      days = { month: (@todays_date + x).month, date:  (@todays_date+x).day, plans: today_plans, wday: wdays[(@todays_date+x).wday]}
 
       
 
 
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+      
 
       @week_days.push(days)
     end
